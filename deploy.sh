969d6111eaafe84e0b4046851b1299bc1b07e9fc
Joe van Bohemen <joseph.vanbohemen@milkbooks.com>
2021-09-11 17:28:09 +1200

    edit site icon and deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
-echo aws s3 cp ~/WORKSPACE/joe/future.hellolennie/frontend/public/
-echo s3://lennie-site-test/ --recursive
-echo --profile joe
+echo "aws s3 cp ~/WORKSPACE/joe/future.hellolennie/frontend/public/"
+echo "s3://lennie-site-test/ --recursive"
+echo "--acl public-read"
+echo "--profile joe"
+echo ""
+echo "------"
+echo ""
 aws s3 cp ~/WORKSPACE/joe/future.hellolennie/frontend/public/ \
 s3://lennie-site-test/ --recursive \
+--acl public-read \
 --profile joe
