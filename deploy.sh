382f61a0b8e9d23ef45e072f5e18d6ce2cd6ac0a
Joe van Bohemen <joseph.vanbohemen@milkbooks.com>
2021-09-12 17:22:56 +1200

    use private bucket

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,13 @@
 echo "aws s3 cp ~/WORKSPACE/joe/future.hellolennie/frontend/public/"
-echo "s3://lennie-site-test/ --recursive"
-echo "--acl public-read"
+echo "s3://lennie-private-test/ --recursive"
+# echo "s3://lennie-site-test/ --recursive"
+# echo "--acl public-read"
 echo "--profile joe"
 echo ""
 echo "------"
 echo ""
 aws s3 cp ~/WORKSPACE/joe/future.hellolennie/frontend/public/ \
-s3://lennie-site-test/ --recursive \
---acl public-read \
+s3://lennie-private-test/ --recursive \
 --profile joe
+# s3://lennie-site-test/ --recursive \
+# --acl public-read \
