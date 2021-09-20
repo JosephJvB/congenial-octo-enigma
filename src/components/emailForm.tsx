ff33143465c04ca491387e947bd263927917d862
Joe van Bohemen <joseph.vanbohemen@milkbooks.com>
2021-09-20 20:53:46 +1200

    add new api lambda url

--- src/components/emailForm.tsx
+++ src/components/emailForm.tsx
@@ -3,7 +3,7 @@ import { useState } from "react"
 import axios from "axios"
 import "../styles/main.css"
 
-const lambdaUrl = "https://toirpbw29h.execute-api.ap-southeast-2.amazonaws.com/v1/email"
+const lambdaUrl = "https://krr0kjmhs8.execute-api.ap-southeast-2.amazonaws.com/v1/email"
 
 interface sendData {
   templateId: string
